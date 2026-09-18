28f559297fba2a355d602d05bfbef6db104482c8
Nikita <nikitabishnoi52@gmail.com>
2026-09-18 11:35:28 +0530

    fix user registration route

--- Controller/email.js
+++ Controller/email.js
@@ -1,29 +1,35 @@
 const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: "nikitabishnoi52@gmail.com",
     pass: "ortoapztskdlgfyk"
   }
 });
-const sendEmail = async (to, subject, text) => {
+
+const sendEmail = async (to, subject, message) => {
   try {
-    const mailOptions = {
+    await transporter.verify();
+
+    const info = await transporter.sendMail({
       from: "nikitabishnoi52@gmail.com",
       to: to,
       subject: subject,
-      text: text
-    };
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully:", info.response);
-    return true;
-  } catch (error) {
-    console.log("Error sending email:", error);
+      text: message
+    });
 
+    console.log("EMAIL SENT:", info.response);
+    return true;
+
+  } catch (error) {
+    console.log("EMAIL ERROR:", error);
     return false;
   }
 };
 
-module.exports = {sendEmail};
-
-
+module.exports = {
+  sendEmail
+};
